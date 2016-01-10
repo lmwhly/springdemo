20b1f11c3faf8d3ccb500f0d8f78e3889bc5e320
123868571 <123868571@qq.com>
2016-01-10 15:32:17 +0800

    no better

--- src/main/java/com/controller/HomeController.java
+++ src/main/java/com/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping("/home")
     public String home(Model model) {
-        model.addAttribute("message", "hello, world");
+        model.addAttribute("message", "hello, world!!!!");
         //return "WEB-INF/views/home.jsp";
         return "home";
     }
